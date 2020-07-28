1e15ec700ee5b4cc3dd00bf5a550f27837ea2f5a
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-07-28 11:05:44 +0200

    day2

---
+++ Day2/m7-d2-Strivazon-lifted-state-master/src/components/Book.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default ({ id, title, imageUrl, price, changeBook, bookSelected }) => (
+  <li
+    onClick={() => changeBook(id)}
+    className={bookSelected === id ? "border-thick card mt-3" : "card mt-3"}
+    key={title}
+    style={{ cursor: "pointer" }}
+  >
+    <div className="media card-body">
+      <img className="book-image" src={imageUrl} alt="book cover" />
+      <div>
+        <p className="card-title font-weight-bold">{title}</p>
+        <p>{price}</p>
+      </div>
+    </div>
+  </li>
+);
