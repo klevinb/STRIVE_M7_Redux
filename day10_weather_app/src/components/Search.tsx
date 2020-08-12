8ddbccceb54de233245126a1c27b36f03741619e
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-08-12 20:20:04 +0200

    usestate added and an icon to send the upper coponent data to search

--- day10_weather_app/src/components/Search.tsx
+++ day10_weather_app/src/components/Search.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
 import { FormControl } from "react-bootstrap";
+import { FaSearchLocation } from "react-icons/fa";
 
 interface Props {
   setSearch: any;
 }
 
 function Search({ setSearch }: Props) {
+  const [location, setLocation]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
+
   const keyPress = (e: any): any => {
     if (e.key === "Enter") {
       setSearch(e.currentTarget.value);
@@ -13,13 +19,19 @@ function Search({ setSearch }: Props) {
   };
 
   return (
-    <FormControl
-      id='searchField'
-      type='text'
-      placeholder='Search'
-      onKeyDown={(e: any): KeyboardEvent => keyPress(e)}
-      className='mt-4'
-    />
+    <>
+      <FormControl
+        id='searchField'
+        type='text'
+        placeholder='Search'
+        onChange={(e: any): void => setLocation(e.currentTarget.value)}
+        onKeyDown={(e: any): KeyboardEvent => keyPress(e)}
+        className='mt-4'
+      />
+      <button style={{ display: "none" }} className='mt-4'>
+        <FaSearchLocation onClick={() => setSearch(location)} />
+      </button>
+    </>
   );
 }
 
